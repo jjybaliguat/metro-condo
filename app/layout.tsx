b3891016c8b0a007304d7e29e3829d9d33991447
jjybaliguat <justinejeraldbaliguat@gmail.com>
2025-06-05 13:42:19 +0800

    updates some prices, added faqs and some condo images

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           </Link>
           <Footer />
         </ReduxProvider>
-        <!--Start of Tawk.to Script-->
+        {/* <!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -53,7 +53,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script-->
+<!--End of Tawk.to Script--> */}
         </body>
     </html>
   )
